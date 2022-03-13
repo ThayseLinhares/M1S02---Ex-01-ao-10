dad1aaea23db2bf9be0c065fca7153e9fa04caee
ThayseLinhares <100707628+ThayseLinhares@users.noreply.github.com>
2022-03-13 16:52:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var nome = window.prompt("Qual o seu nome?");
+
+alert("Bem vindo(a) "+nome+ " ao melhor curso de marketing digital!");
+
+function enviarMensagem(){
+alert ("Olá seu cadastro foi enviado com sucesso!");
+
+}
+
+function checar(obg){
+    var checado = obg.checked;
+    if(checado){
+        window.confirm("Estou ciente!");
+    }
+
+    
+}
+
+
